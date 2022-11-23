fbbed6175594f8b93d9b7e3d6a4bfbb335c3af9b
FJJT <934206219@qq.com>
2022-11-23 17:27:28 +0800

    per-avg(HF)

--- system/flcore/clients/clientperavg.py
+++ system/flcore/clients/clientperavg.py
@@ -3,6 +3,8 @@ import torch
 import time
 import copy
 import torch.nn as nn
+from typing import Tuple, Union
+from collections import OrderedDict
 from flcore.optimizers.fedoptimizer import PerAvgOptimizer
 from flcore.clients.clientbase import Client
 
@@ -13,6 +15,7 @@ class clientPerAvg(Client):
 
         # self.beta = args.beta
         self.beta = self.learning_rate
+        self.alpha =  self.learning_rate
 
         self.loss = nn.CrossEntropyLoss()
         self.optimizer = PerAvgOptimizer(self.model.parameters(), lr=self.learning_rate)
@@ -30,9 +33,10 @@ class clientPerAvg(Client):
 
         for step in range(max_local_steps):  # local update
             for X, Y in trainloader:
-                temp_model = copy.deepcopy(list(self.model.parameters()))
+                # temp_model = copy.deepcopy(list(self.model.parameters()))
+                temp_model = copy.deepcopy(self.model)
 
-                # step 1
+                # step 1，计算梯度并更新参数
                 if type(X) == type([]):
                     x = [None, None]
                     x[0] = X[0][:self.batch_size].to(self.device)
@@ -40,15 +44,11 @@ class clientPerAvg(Client):
                 else:
                     x = X[:self.batch_size].to(self.device)
                 y = Y[:self.batch_size].to(self.device)
-                if self.train_slow:
-                    time.sleep(0.1 * np.abs(np.random.rand()))
-                self.optimizer.zero_grad()
-                output = self.model(x)
-                loss = self.loss(output, y)
-                loss.backward()
-                self.optimizer.step()
-
-                # step 2
+                data_batch_1 = (x, y)
+                grads = self.compute_grad(temp_model, data_batch_1)
+                for param, grad in zip(temp_model.parameters(), grads):
+                    param.data.sub_(self.alpha * grad)
+                # step 2,只计算梯度，没有更新参数
                 if type(X) == type([]):
                     x = [None, None]
                     x[0] = X[0][self.batch_size:].to(self.device)
@@ -56,18 +56,33 @@ class clientPerAvg(Client):
                 else:
                     x = X[self.batch_size:].to(self.device)
                 y = Y[self.batch_size:].to(self.device)
-                if self.train_slow:
-                    time.sleep(0.1 * np.abs(np.random.rand()))
-                self.optimizer.zero_grad()
-                output = self.model(x)
-                loss = self.loss(output, y)
-                loss.backward()
+                data_batch_2 = (x, y)
+                grads_1st = self.compute_grad(temp_model, data_batch_2)
 
-                # restore the model parameters to the one before first update
-                for old_param, new_param in zip(self.model.parameters(), temp_model):
-                    old_param.data = new_param.data.clone()
+                # step 3,计算 hessian矩阵，得到二阶偏倒数
 
-                self.optimizer.step(beta=self.beta)
+                if type(X) == type([]):
+                    x = [None, None]
+                    x[0] = X[0][self.batch_size:].to(self.device)
+                    x[1] = X[1][self.batch_size:]
+                else:
+                    x = X[self.batch_size:].to(self.device)
+                y = Y[self.batch_size:].to(self.device)
+                data_batch_3 = (x, y)
+                grads_2nd = self.compute_grad(self.model, data_batch_3, v=grads_1st, second_order_grads=True)
+
+                # step 4,公式有问题
+                for param, grad1, grad2 in zip(self.model.parameters(), grads_1st, grads_2nd):
+                    # param.data.sub_(self.beta * grad1 - self.beta * self.alpha * grad2)
+                    param.data.sub_(self.beta * grad1 - self.beta * self.alpha * grad2 * grad1)
+
+
+
+                # # restore the model parameters to the one before first update
+                # for old_param, new_param in zip(self.model.parameters(), temp_model):
+                #     old_param.data = new_param.data.clone()
+                #
+                # self.optimizer.step(beta=self.beta)
 
         # self.model.cpu()
 
@@ -108,3 +123,72 @@ class clientPerAvg(Client):
         self.optimizer.step(beta=self.beta)
 
         # self.model.cpu()
+
+    def get_hessian(self, x, y, model):
+        # ind = np.random.randint(0, high=len(data), size=None, dtype=int)
+        # seq, label = data[ind]
+        seq = x
+        label = y
+        y_pred = model(seq)
+        loss_function = self.loss
+        loss = loss_function(y_pred, label)
+        grads = torch.autograd.grad(loss, model.parameters(), retain_graph=True, create_graph=True)
+        hessian_params = []
+        for k in range(len(grads)):
+            hess_params = torch.zeros_like(grads[k])
+            for i in range(grads[k].size(0)):
+                # w or b?
+                if len(grads[k].size()) == 2:
+                    for j in range(grads[k].size(1)):
+                        hess_params[i, j] = \
+                        torch.autograd.grad(grads[k][i][j], model.parameters(), retain_graph=True)[k][i, j]
+                else:
+                    hess_params[i] = torch.autograd.grad(grads[k][i].sum(), model.parameters(), retain_graph=True)[k][i]
+            hessian_params.append(hess_params)
+
+        return hessian_params
+
+    def compute_grad(
+            self,
+            model: torch.nn.Module,
+            data_batch: Tuple[torch.Tensor, torch.Tensor],
+            v: Union[Tuple[torch.Tensor, ...], None] = None,
+            second_order_grads=False,
+    ):
+        x, y = data_batch
+        if second_order_grads:
+            frz_model_params = copy.deepcopy(model.state_dict())
+            delta = 1e-3
+            dummy_model_params_1 = OrderedDict()
+            dummy_model_params_2 = OrderedDict()
+            with torch.no_grad():
+                for (layer_name, param), grad in zip(model.named_parameters(), v):
+                    dummy_model_params_1.update({layer_name: param + delta * grad})
+                    dummy_model_params_2.update({layer_name: param - delta * grad})
+
+            model.load_state_dict(dummy_model_params_1, strict=False)
+            logit_1 = model(x)
+            # loss_1 = self.criterion(logit_1, y) / y.size(-1)
+            loss_1 = self.loss(logit_1, y)
+            grads_1 = torch.autograd.grad(loss_1, model.parameters())
+
+            model.load_state_dict(dummy_model_params_2, strict=False)
+            logit_2 = model(x)
+            loss_2 = self.loss(logit_2, y)
+            # loss_2 = self.criterion(logit_2, y) / y.size(-1)
+            grads_2 = torch.autograd.grad(loss_2, model.parameters())
+
+            model.load_state_dict(frz_model_params)
+
+            grads = []
+            with torch.no_grad():
+                for g1, g2 in zip(grads_1, grads_2):
+                    grads.append((g1 - g2) / (2 * delta))
+            return grads
+
+        else:
+            logit = model(x)
+            # loss = self.criterion(logit, y) / y.size(-1)
+            loss = self.loss(logit, y)
+            grads = torch.autograd.grad(loss, model.parameters())
+            return grads
